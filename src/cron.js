691a2623b271667000d7d1c04b4f35be7e201df7
Ivan Tee <e0319256@u.nus.edu>
2021-01-09 03:53:49 +0800

    Complete cron.js

--- src/cron.js
+++ src/cron.js
@@ -1,23 +1,67 @@
 require("dotenv").config();
 const db = require("better-sqlite3")("main.db");
-
-const cron = require('node-cron');
-cron.schedule("3 * * * * *", sendReminders)
-
-function sendReminders() {
-  const row = db.prepare("SELECT userID FROM users");
-  console.log(row.all());
-  console.log("hello world!");
-}
-
-
-/*
+const axios = require('axios').default;
 const { tapiFF } = require("yatbl");
 const tapi = tapiFF(process.env.BOT_TOKEN);
 
-tapi("sendMessage", {
-  chat_id,
-  text:
-    "Missing registration token! Go luminus to setup your notification integration",
-});
+const cron = require('node-cron');
+cron.schedule("*/5 * * * * *", sendReminders)
+
+function sendReminders() {
+  console.log("test")
+  try {
+    const row = db.prepare("SELECT userID, token, chat_id FROM users");
+    row.all()
+    .forEach(async (row) => {
+      var toBeSent = "You need complete these soon!\n"
+
+      const response = await axios.get(`http://localhost:3001/task/`, {
+      headers : {
+        'auth-token' : '8073e7cd'
+      }})
+        response.data.forEach((row) => {
+          if(!row.completed) {
+            console.log(row)
+            toBeSent += row.module + " - " + row.taskName + " - " + row.endDate + "\n"
+          }
+        })
+      
+        console.log(row.chat_id)
+      tapi("sendMessage", {
+        chat_id: row.chat_id,
+        text:toBeSent,
+      });
+        
+      console.log("Successfully Sent");
+    }
+    );
+  } catch (err) {
+    console.log(err.message)
+  }
+
+  //const row = db.prepare("SELECT userID FROM users");
+  //console.log(row)
+  //row.all().forEach((row) => {
+    
+  //});
+}
+
+/*
+axios.get(`http://localhost:3001/task/`, {
+      headers : {
+        'auth-token' : '8073e7cd'
+      }
+    })
+    .then((response) => 
+      response
+      .data
+      .forEach((row) => {
+        if(!row.completed) {
+          console.log(row)
+        }
+      })));
+*/
+
+/*
+
 */
