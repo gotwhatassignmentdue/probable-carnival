d9923d087391e36d6e474e33217b07171e1c7f52
Jaime <jaimeloeuf@gmail.com>
2021-01-09 10:36:00 +0800

    Clean up cron script.

--- src/cron.js
+++ src/cron.js
@@ -1,67 +1,50 @@
 require("dotenv").config();
 const db = require("better-sqlite3")("main.db");
-const axios = require('axios').default;
+const axios = require("axios").default;
 const { tapiFF } = require("yatbl");
 const tapi = tapiFF(process.env.BOT_TOKEN);
 
-const cron = require('node-cron');
-cron.schedule("*/5 * * * * *", sendReminders)
+const cron = require("node-cron");
+cron.schedule("*/5 * * * * *", pollAndSendReminders);
 
-function sendReminders() {
-  console.log("test")
+function pollAndSendReminders() {
+  console.log("Polling luminus API for tasks");
+
   try {
+    // Create statement to load all users from DB
     const row = db.prepare("SELECT userID, token, chat_id FROM users");
-    row.all()
-    .forEach(async (row) => {
-      var toBeSent = "You need complete these soon!\n"
 
+    // Poll API for every single user
+    row.all().forEach(async function (row) {
       const response = await axios.get(`http://localhost:3001/task/`, {
-      headers : {
-        'auth-token' : '8073e7cd'
-      }})
-        response.data.forEach((row) => {
-          if(!row.completed) {
-            console.log(row)
-            toBeSent += row.module + " - " + row.taskName + " - " + row.endDate + "\n"
-          }
-        })
-      
-        console.log(row.chat_id)
-      tapi("sendMessage", {
-        chat_id: row.chat_id,
-        text:toBeSent,
+        headers: {
+          // "auth-token": "8073e7cd",
+          "auth-token": row.userID,
+        },
       });
-        
-      console.log("Successfully Sent");
-    }
-    );
-  } catch (err) {
-    console.log(err.message)
+
+      const toBeSent = [];
+
+      // Loop through all the tasks to find tasks that have yet to be completed
+      // Push the message with a newline into the array of tasks to update user with
+      response.data.forEach(function (task) {
+        if (!task.completed)
+          toBeSent.push(
+            task.module + " - " + task.taskName + " - " + task.endDate + "\n"
+          );
+      });
+
+      // If there are any upcoming tasks
+      if (toBeSent.length) {
+        await tapi("sendMessage", {
+          chat_id: row.chat_id,
+          text: "You need complete these soon!\n" + toBeSent.join(""),
+        });
+
+        console.log(`Notification for user ${row.userID} sent!`);
+      } else console.log(`No upcoming tasks for user ${row.userID}!`);
+    });
+  } catch (error) {
+    console.error(error);
   }
-
-  //const row = db.prepare("SELECT userID FROM users");
-  //console.log(row)
-  //row.all().forEach((row) => {
-    
-  //});
 }
-
-/*
-axios.get(`http://localhost:3001/task/`, {
-      headers : {
-        'auth-token' : '8073e7cd'
-      }
-    })
-    .then((response) => 
-      response
-      .data
-      .forEach((row) => {
-        if(!row.completed) {
-          console.log(row)
-        }
-      })));
-*/
-
-/*
-
-*/
